48e3687754e9ab41f3e80697374003c8fcf4549e
Jungang <626967215@qq.com>
2022-12-22 15:22:14 +0800

    Runsh

--- experiments/image_classification/run.sh
+++ experiments/image_classification/run.sh
@@ -1,6 +1,6 @@
-for eps in 0.5
+for eps in 0.5 1
 do
-    CUDA_VISIBLE_DEVICES=6,7 python run_experiment.py --config=configs/cifar10_wrn_16_4_eps${eps}_top10.py --jaxline_mode=train_eval_multithreaded
+    CUDA_VISIBLE_DEVICES=6,7 python run_experiment.py --config=configs/cifar10_wrn_28_10_eps${eps}_fintune_top10.py --jaxline_mode=train_eval_multithreaded
 done
 # CUDA_VISIBLE_DEVICES=2 python run_experiment.py --config=configs/project/mlp_eps1_baseline.py --jaxline_mode=train_eval_multithreaded
 # CUDA_VISIBLE_DEVICES=0,1 python run_experiment.py --config=configs/resnet18/cifar10_resnet18_eps1_lr4_top10.py --jaxline_mode=train_eval_multithreaded
